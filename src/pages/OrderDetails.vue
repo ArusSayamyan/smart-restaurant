1021cf2d85c0f151faa81ec08cb5aa7dc6ca84aa
Arus Sayamyan <arus.sayamyan@codetime.am>
2023-06-18 19:45:56 +0400

    add selected products to orderList

--- src/pages/OrderDetails.vue
+++ src/pages/OrderDetails.vue
@@ -2,16 +2,20 @@
   <base-wrapper>
     <div class="orderDetails">
       <div class="orderDetails__info">
-        <div class="orderDetails__nav">nav</div>
+        <div class="orderDetails__nav">
+          <span>{{ object.statue }} {{ object.name }}</span>
+          <span>{{ date }}</span>
+        </div>
         <div class="orderDetails__check">
           <div class="orderDetails__header">
             <h1>check</h1>
             <span v-if="select.length > 0">total price is {{ doubleCount }}$</span>
           </div>
           <table v-if="select.length > 0">
-            <tr  v-for="sel in select" :key="sel.name">
+            <tr v-for="sel in select" :key="sel.name">
               <td>{{ sel.name }}</td>
-              <td>{{ sel.price }}$</td>
+              <td>{{ sel.count }}</td>
+              <td>{{ sel.count * +sel.price }}$</td>
             </tr>
           </table>
         </div>
@@ -23,11 +27,13 @@
               <AccordionTab header="Kitchen">
                 <Accordion>
                   <AccordionTab header="burgers">
-                    <OrderList  v-model="products" listStyle="height:auto" dataKey="id" v-model:selection="selection" @click="selected">
-                      <template> List of Products </template>
+                    <OrderList v-model="products" listStyle="height:auto" dataKey="id" v-model:selection="selection"
+                               @click="selected">
+                      <template> List of Products</template>
                       <template #item="slotProps">
                         <div class="flex flex-wrap p-2 align-items-center gap-3">
-                          <img class="w-4rem shadow-2 flex-shrink-0 border-round" :src="slotProps.item.img" :alt="slotProps.item.name" />
+                          <img class="w-4rem shadow-2 flex-shrink-0 border-round" :src="slotProps.item.img"
+                               :alt="slotProps.item.name"/>
                           <div class="flex-1 flex flex-column gap-2">
                             <span class="font-bold">{{ slotProps.item.name }}</span>
                           </div>
@@ -39,11 +45,13 @@
                 </Accordion>
               </AccordionTab>
               <AccordionTab header="Bar">
-                <OrderList v-model="products" listStyle="height:auto" dataKey="id" v-model:selection="selection" @click="selected">
-                  <template #header> List of Products </template>
+                <OrderList v-model="products" listStyle="height:auto" dataKey="id" v-model:selection="selection"
+                           @click="selected">
+                  <template #header> List of Products</template>
                   <template #item="slotProps">
                     <div class="flex flex-wrap p-2 align-items-center gap-3">
-                      <img class="w-4rem shadow-2 flex-shrink-0 border-round" :src="slotProps.item.img" :alt="slotProps.item.name" />
+                      <img class="w-4rem shadow-2 flex-shrink-0 border-round" :src="slotProps.item.img"
+                           :alt="slotProps.item.name"/>
                       <div class="flex-1 flex flex-column gap-2">
                         <span class="font-bold">{{ slotProps.item.name }}</span>
                         <div class="flex align-items-center gap-2">
@@ -72,7 +80,7 @@ import OrderList from 'primevue/orderlist';
 import img from '@/assets/logo.svg';
 
 
-import { ref, computed } from 'vue';
+import {ref, computed} from 'vue';
 
 const selection = ref()
 const select = ref([])
@@ -80,51 +88,82 @@ const products = ref([
   {
     name: 'product1',
     price: '55',
-    img: img
-
+    img: img,
+    count: 0
   },
   {
     name: 'product2',
     price: '60',
-    img: img
-
-
+    img: img,
+    count: 0
   },
   {
     name: 'product3',
     price: '20',
-    img: img
+    img: img,
+    count: 0
   },
   {
     name: 'product4',
     price: '55',
-    img: img
+    img: img,
+    count: 0
 
   },
   {
     name: 'product5',
     price: '60',
-    img: img
-
-
+    img: img,
+    count: 0
   },
   {
     name: 'product6',
     price: '20',
-    img: img
+    img: img,
+    count: 0
   }
 ]);
 
-function selected() {
-  select.value.push(...selection.value)
-}
+const object = JSON.parse(localStorage.getItem('name'))
 
-const doubleCount = computed(() => {
-  return select.value.reduce(
-      (accumulator, currentValue) => accumulator + +currentValue.price,
-      0
-  );
-});
+// ADD SELECTED PRODUCT TO LIST
+function selected() {
+  if (!select.value.includes(...selection.value)) {
+    select.value.push(...selection.value)
+    for (let item of selection.value) {
+      item.count = 1
+    }
+  } else {
+    for (let item of select.value) {
+      for (let sel of selection.value) {
+        if (item === sel) {
+          item.count++
+        }
+      }
+
+    }
+  }}
+
+  const doubleCount = computed(() => {
+    return select.value.reduce(
+        (accumulator, currentValue) => accumulator + (currentValue.count * (+currentValue.price)),
+        0
+    );
+  });
+
+//date
+  const date = computed(() => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1; // Months are zero-based, so add 1
+    const day = today.getDate();
+
+// Get time
+    const hours = today.getHours();
+    const minutes = today.getMinutes();
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  })
 </script>
 
 <style scoped lang="scss">
@@ -133,6 +172,15 @@ const doubleCount = computed(() => {
   justify-content: space-between;
   flex-wrap: wrap;
   height: 100vh;
+
+  &__nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #000000;
+    color: #ffffff;
+    padding: 15px;
+  }
 
   &__info {
     flex-basis: 50%;
@@ -153,6 +201,7 @@ const doubleCount = computed(() => {
     align-items: center;
   }
 }
+
 .w-4rem.shadow-2.flex-shrink-0.border-round {
   width: 40px !important;
 }
@@ -162,9 +211,11 @@ const doubleCount = computed(() => {
   justify-content: space-between;
   align-items: center;
 }
+
 table {
   width: 100%;
 }
+
 table tr:last-child {
   background-color: #000000;
   color: #ffffff;
