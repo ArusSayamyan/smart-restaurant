24f1a2d70ee801a7ffcbd2256c3f266c8c1c1735
Arus Sayamyan <arus.sayamyan@codetime.am>
2023-07-13 15:40:08 +0400

    add some responsive style

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,45 +1,53 @@
 <template>
-<div class="homePage">
-  <base-wrapper>
-    <div class="homePage__signIn">
-      <form action="" class="homePage__signInForm" @submit.prevent="checkCode">
-        <h3 class="homePage__formTitle">enter your access code</h3>
-        <div class="homePage__inputBlock">
-          <input type="number" required class="homePage__pin" placeholder="your code" v-model="enteredCode">
-          <p class="homePage__codeNotFound" v-if="codeNotFound">This code not found</p>
-        </div>
-        <button class="homePage__btn">Login</button>
-      </form>
-    </div>
-  </base-wrapper>
-</div>
+  <div class="homePage">
+    <base-wrapper>
+      <div class="homePage__signIn">
+        <form action="" class="homePage__signInForm" @submit.prevent="checkCode">
+          <h3 class="homePage__formTitle">enter your access code</h3>
+          <div class="homePage__inputBlock">
+            <input type="number" required class="homePage__pin" placeholder="your code" v-model="enteredCode">
+            <p class="homePage__codeNotFound" v-if="codeNotFound">This code not found</p>
+            <p class="homePage__codeNotFound" v-if="isMobile">Can't be accessed by phone</p>
+          </div>
+          <button class="homePage__btn">Login</button>
+        </form>
+      </div>
+    </base-wrapper>
+  </div>
 </template>
 
 <script setup>
 import {computed, onMounted, ref} from 'vue';
-import { useRouter } from 'vue-router'
-import { useStore } from 'vuex'
+import {useRouter} from 'vue-router'
+import {useStore} from 'vuex'
 import data from '@/json/data.json'
+
 const enteredCode = ref();
 const status = ref('');
 const codeNotFound = ref(false)
+const isMobile = ref(false)
 const router = useRouter()
 const store = useStore()
+
 function checkCode() {
-  for(let obj of data) {
-    if(enteredCode.value === +obj.code){
-      store.commit('changeLoginId', obj.id)
-      status.value = obj.status
-      enteredCode.value = null;
-      router.push('/' + status.value + '/' + obj.id)
-      localStorage.setItem('name', JSON.stringify({
-        name: obj.name,
-        statue: obj.status,
-        id: obj.id
-      }))
+  const elem = data.find(item => +item.code === enteredCode.value)
+  if (elem !== undefined) {
+    if(elem.status !== 'waiter' && window.outerWidth < 420) {
+      isMobile.value = true;
     }else {
-      codeNotFound.value = true
+      store.commit('changeLoginId', elem.id)
+      status.value = elem.status
+      enteredCode.value = null;
+      router.push('/' + status.value + '/' + elem.id)
+      localStorage.setItem('name', JSON.stringify({
+        name: elem.name,
+        statue: elem.status,
+        id: elem.id
+      }))
     }
+
+  } else {
+    codeNotFound.value = true
   }
 }
 
@@ -47,11 +55,15 @@ const prod = computed(() => {
   return store.getters.getProductList
 });
 
-onMounted(()=> {
-  if(localStorage.getItem('allProducts')) {
+// function isMobileDevice() {
+//   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+// }
+
+onMounted(() => {
+  if (localStorage.getItem('allProducts')) {
     const products = JSON.parse(localStorage.getItem('allProducts'))
     localStorage.setItem('allProducts', JSON.stringify(products))
-  }else {
+  } else {
     localStorage.setItem('allProducts', JSON.stringify(prod.value))
   }
 })
